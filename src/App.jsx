e03fa0181122a4fb6345cb14280002efcddb5bad
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-01-12 21:24:18 +0330

    Add Context

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import Cart from "./pages/Cart";
 import NoPage from "./pages/NoPage";
 import ProductDetailes from "./pages/ProductDetailes";
 import { useReducer } from "react";
+// Import Contexts
+import ShopContext from "./context/ShopContext";
 
 // Set Reducer Values
 const initialState = {
 	isLoading: true,
 	products: [],
 };
-const reducer = (state, {payload , type}) => {
+const reducer = (data, { payload, type }) => {
 	switch (type) {
 		case "fetchAPI":
-			return { ...state, products: payload };
+			return { ...data, products: payload };
 	}
 };
 
@@ -24,16 +26,18 @@ export default function App() {
 	const [data, dispatch] = useReducer(reducer, initialState);
 
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Layout />}>
-					<Route index element={<Navigate to="/products" />} />
-					<Route path="/products" element={<Products data={data} dispatch={dispatch} />} />
-					<Route path="/products/:id" element={<ProductDetailes />} />
-					<Route path="/cart" element={<Cart />} />
-					<Route path="/*" element={<NoPage />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
+		<ShopContext.Provider value={{ data, dispatch }}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<Route index element={<Navigate to="/products" />} />
+						<Route path="/products" element={<Products data={data} dispatch={dispatch} />} />
+						<Route path="/products/:id" element={<ProductDetailes />} />
+						<Route path="/cart" element={<Cart />} />
+						<Route path="/*" element={<NoPage />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</ShopContext.Provider>
 	);
 }
