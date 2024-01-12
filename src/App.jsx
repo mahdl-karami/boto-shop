a751c3ee88b5d28828da16c8d3d9871ac1303d94
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-01-12 18:33:45 +0330

    Fetch API By Axios

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,29 @@ import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import NoPage from "./pages/NoPage";
 import ProductDetailes from "./pages/ProductDetailes";
+import { useReducer } from "react";
+
+// Set Reducer Values
+const initialState = {
+	isLoading: true,
+	products: [],
+};
+const reducer = (state, {payload , type}) => {
+	switch (type) {
+		case "fetchAPI":
+			return { ...state, products: payload };
+	}
+};
 
 export default function App() {
+	const [data, dispatch] = useReducer(reducer, initialState);
+
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Layout />}>
 					<Route index element={<Navigate to="/products" />} />
-					<Route path="/products" element={<Products />} />
+					<Route path="/products" element={<Products data={data} dispatch={dispatch} />} />
 					<Route path="/products/:id" element={<ProductDetailes />} />
 					<Route path="/cart" element={<Cart />} />
 					<Route path="/*" element={<NoPage />} />
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,3 +1,18 @@
-export default function Products() {
-	return <div>Products</div>;
+/* eslint-disable react-hooks/exhaustive-deps */
+// Import Libraryes
+import { useEffect } from "react";
+import { API } from "../services/AxiosConfig";
+
+export default function Products({ dispatch , data }) {
+	useEffect(() => {
+        const getProducts = async ()=>{
+            // Fetching API By Axios From AxiosConfig.js
+            const res = await API.get("/products");
+            dispatch({type: "fetchAPI" , payload: res})
+        }
+		getProducts();
+	}, []);
+	return <div>
+        <button onClick={()=> console.log(data)}>Log API Response</button>
+    </div>;
 }
