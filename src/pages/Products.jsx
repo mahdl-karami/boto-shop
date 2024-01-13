c8f431fbc231e3a40811c01aaa3fb26dcb19d112
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-01-13 12:46:25 +0330

    Add Loader

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -11,6 +11,7 @@ import { ShopContext } from "../context/ShopContext";
 import ProductCard from "../components/ProductCard";
 import styles from "../styles/products.module.css";
 import { TbCategory } from "react-icons/tb";
+import { RotatingLines } from "react-loader-spinner";
 
 export default function Products() {
 	const {
@@ -20,23 +21,30 @@ export default function Products() {
 		<>
 			<SearchBar />
 			<div className={styles.products}>
-				<div className={styles.productsList}>
-					{products.map((product) => (
-						<ProductCard key={product.id} product={product} />
-					))}
-				</div>
+				{products.length ? (
+					<div className={styles.productsList}>
+						{products.map((product) => (
+							<ProductCard key={product.id} product={product} />
+						))}
+					</div>
+				) : (
+					<div className={styles.loading}>
+						<RotatingLines visible={true} height="200" width="200" color="grey" strokeWidth="5" strokeColor="#6433ff" animationDuration="0.75" ariaLabel="rotating-lines-loading"  />
+					</div>
+				)}
+
 				<div className={styles.category}>
-						<span>
-							<TbCategory />
-							<p>Categoris</p>
-						</span>
-						<ul>
-							<li>All</li>
-							<li>Electronics</li>
-							<li>Jewelery</li>
-							<li>{"men's"}</li>
-							<li>{"women's"}</li>
-						</ul>
+					<span>
+						<TbCategory />
+						<p>Categoris</p>
+					</span>
+					<ul>
+						<li>All</li>
+						<li>Electronics</li>
+						<li>Jewelery</li>
+						<li>{"men's"}</li>
+						<li>{"women's"}</li>
+					</ul>
 				</div>
 			</div>
 		</>
