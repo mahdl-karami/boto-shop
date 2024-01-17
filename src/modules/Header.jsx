11814ecb3953934befbefeea11a7e625f254454a
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-01-17 17:11:20 +0330

    Clear Filter By Logo

--- src/modules/Header.jsx
+++ src/modules/Header.jsx
@@ -1,3 +1,5 @@
+// Import Hooks
+import { useShopContext } from "../context/ShopContext";
 // Import Modules
 import styles from "../styles/layout.module.css";
 // Import Lybraries
@@ -5,10 +7,16 @@ import { FaOpencart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+	const { dispatch } = useShopContext();
 	return (
 		<header className={styles.layout}>
-			<h1>
-				<a href="products">BotoShop</a>
+			<h1
+				onClick={() => {
+					dispatch({ type: "setQuery", payload: { name: "search", value: "" } });
+					dispatch({ type: "setQuery", payload: { name: "category", value: "all" } });
+				}}
+			>
+				<Link to="/">BotoShop</Link>
 			</h1>
 			<Link to="/cart">
 				<button>
