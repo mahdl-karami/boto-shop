9af203f772945397611b01bba9a72fa97e055527
Mohammad Mahdi <mo.mahdi.ka@gmail.com>
2024-01-18 14:42:18 +0330

    Style For Card Panel

--- src/components/CardPanel.jsx
+++ src/components/CardPanel.jsx
@@ -1,13 +1,14 @@
 // Import Lybraries
 import { TbShoppingBagCheck, TbListDetails } from "react-icons/tb";
 import { RiDeleteBinLine } from "react-icons/ri";
+import { HiOutlineMinusSm, HiOutlinePlus } from "react-icons/hi";
 
 // Import Modules
 import styles from "../styles/card.module.css";
 import { Link } from "react-router-dom";
 import { decreaseHandler, increaseHandler } from "../Helpers/CartHandler";
 
-export default function CardPanel({ count, setCount, removeHandler , id}) {
+export default function CardPanel({ count, setCount, removeHandler, id }) {
 	return (
 		<div className={styles.icons}>
 			<Link to={`/products/${id}`}>
@@ -16,7 +17,7 @@ export default function CardPanel({ count, setCount, removeHandler , id}) {
 				</button>
 			</Link>
 
-			<div>
+			<div className={styles.panel}>
 				{!count && (
 					<button onClick={() => increaseHandler(setCount)}>
 						<TbShoppingBagCheck />
@@ -34,10 +35,14 @@ export default function CardPanel({ count, setCount, removeHandler , id}) {
 								<RiDeleteBinLine />
 							</button>
 						) : (
-							<button onClick={() => decreaseHandler(setCount)}>-</button>
+							<button onClick={() => decreaseHandler(setCount)}>
+								<HiOutlineMinusSm />
+							</button>
 						)}
-						<button>{count}</button>
-						<button onClick={() => increaseHandler(setCount)}>+</button>
+						<p>{count}</p>
+						<button onClick={() => increaseHandler(setCount)}>
+							<HiOutlinePlus />
+						</button>
 					</div>
 				)}
 			</div>
